67bfcb725b42ae57753741fdb37478f546e76ec2
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-18 15:56:44 +0530

    backend integrate

---
+++ backend/src/test/java/com/ramya/ramya/RamyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramya.ramya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
