929c5b93f9420677a7b551a4d4d66de402b0124b
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-21 19:59:45 +0530

    add ProductOrder Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,27 +10,25 @@ import BulkOrder from "./components/bulkOrder/BulkOrder";
 import AccountLogin from "./pages/AccountLogin";
 import { useSelector } from "react-redux";
 import Cart from "./pages/Cart";
-
+import ProductOrder from "./components/products/ProductOrder";
 
 function App() {
   const isCartOpen = useSelector((state) => state.navBar.cartOpen);
   return (
     <>
-        {console.log(isCartOpen, "cart open")}
-        {isCartOpen ? <Cart /> : null}
-        <Routes>
-          <Route path="/collections" element={<HomePage />} />
-          <Route path="/collections/men" element={<MenCollections/>} />
-          <Route path="/collections/women" element={<WomenCollections/>} />
-          <Route path="/collections/ecoflex" element={<Ecoflex/>} />
-          <Route path="/collections/stethoscope" element={<Stethoscope/>} />
-          <Route path="/collections/track-order" element={<TrackOrder/>} />
-          <Route path="/collections/bulk-order" element={<BulkOrder/>} />
-          <Route path="/account/login" element={<AccountLogin/>} />
-          <Route path="/account/login" element={<AccountLogin/>} />
-
-        </Routes>
-  
+      {console.log(isCartOpen, "cart open")}
+      {isCartOpen ? <Cart /> : null}
+      <Routes>
+        <Route path="/collections" element={<HomePage />} />
+        <Route path="/collections/men" element={<MenCollections />} />
+        <Route path="/collections/women" element={<WomenCollections />} />
+        <Route path="/collections/ecoflex" element={<Ecoflex />} />
+        <Route path="/collections/stethoscope" element={<Stethoscope />} />
+        <Route path="/collections/track-order" element={<TrackOrder />} />
+        <Route path="/collections/bulk-order" element={<BulkOrder />} />
+        <Route path="/account/login" element={<AccountLogin />} />
+        <Route path="/collections/products" element={<ProductOrder />} />
+      </Routes>
     </>
   );
 }
