d2e27f97f0b458110eb8e00d2dfbdf302fda8515
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-22 15:06:27 +0530

    add Links

--- frontend/src/components/homePage/CategorySection.jsx
+++ frontend/src/components/homePage/CategorySection.jsx
@@ -7,6 +7,7 @@ import forestgreen from "../../assets/forestgreen.png";
 import olive from "../../assets/olive.png";
 import galaxyblue from "../../assets/galaxyblue.png";
 import hotpink from "../../assets/hotpink.png";
+import { Link } from "react-router-dom";
 
 function CategorySection() {
   const categories = [
@@ -24,12 +25,12 @@ function CategorySection() {
       <div className="bycategory baseTextColor">
         <h1>Shop By Categories</h1>
         <div className="categoryList">
-          <li className="mens">MENS</li>
-          <li className="womens">WOMENS</li>
-          <li className="ecoflex">ECOFLEX</li>
-          <li className="stethoscope">STETHOSCOPE</li>
-          <li className="aprons">APRONS</li>
-          <li className="multipacks">MULTI PACKS</li>
+          <Link className="Link" to="/collections/men"><li className="mens">MENS</li></Link>
+          <Link className="Link" to="/collections/women"><li className="womens">WOMENS</li></Link>
+          <Link className="Link" to="/"><li className="ecoflex">ECOFLEX</li></Link>
+          <Link className="Link" to="/"><li className="stethoscope">STETHOSCOPE</li></Link>
+          <Link className="Link" to="/"><li className="aprons">APRONS</li></Link>
+          <Link className="Link" to="/"><li className="multipacks">MULTI PACKS</li></Link>
         </div>
       </div>
       <div className="byColor baseTextColor">
@@ -37,7 +38,7 @@ function CategorySection() {
         <ul className="colorList">
           {categories.map((category, index) => (
             <li className={category.name} key={index}>
-              <img src={category.image} alt="" width="150px"/>
+              <img src={category.image} alt={category.name} width="150px"/>
               <h4>{category.name}</h4>
             </li>
           ))}
