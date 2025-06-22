37f6a2218c24f20f4b20ba60cbc215669a95a572
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-22 21:28:30 +0530

    cart updated

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,8 @@ import { MdOutlineClose } from "react-icons/md";
 import { cartOpen } from "../slices/NavBarSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import "../styles/homepage/Cart.css";
+import { MdDeleteForever } from "react-icons/md";
 
 function Cart() {
   const dispatch = useDispatch();
@@ -9,46 +11,41 @@ function Cart() {
   console.log("Selected Products:", selectedProducts);
   return (
     <div className="cart-section">
-      <div className="cart-section2">
-        <span className="close-btn " onClick={() => dispatch(cartOpen())}>
-          <MdOutlineClose />
-        </span>
-        <div className="cart-empty">
-          <p>Cart is Empty</p>
-          <div>
-            {selectedProducts && selectedProducts.length > 0 ? (
-              <div className="productOrderContainer">
-                <h1>Order Summary</h1>
-                <ul className="productList">
-                  {selectedProducts.map((product) => (
-                    <li key={product.id} className="productItem">
-                      <img
-                        src={product.image}
-                        alt={product.name}
-                        width="100px"
-                      />
-                      <div className="productDetails">
-                        <h3>{product.name}</h3>
-                        <p>Price: ₹{product.price}</p>
-                        <p>Color: {product.color}</p>
-                        <p>Quantity: {product.quantity}</p>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              <p>No products in the cart.</p>
-            )}
+      <span className="close-btn " onClick={() => dispatch(cartOpen())}>
+        <MdOutlineClose />
+      </span>
+
+      <div className="cart">
+        {selectedProducts && selectedProducts.length > 0 ? (
+          <div className="productOrderContainer">
+            <ul className="productList">
+              {selectedProducts.map((product) => (
+                <li key={product.id} className="productItem">
+                  <img src={product.image} alt={product.name} width="100px" />
+                  <div className="productDetails">
+                    <div className="productInfo">
+                      <h3>{product.name}</h3>
+                      <p>Price: ₹{product.price}</p>
+                      <p>Color: {product.color}</p>
+                    </div>
+                    <div className="action-btn">
+                      <p><MdDeleteForever /></p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-          <Link
-            className="Link"
-            to={"/collections"}
-            onClick={() => dispatch(cartOpen())}
-          >
-            <p className="shopping-btn">Continue Shopping &rarr;</p>
-          </Link>
-        </div>
+        ) : (
+          <p>No products in the cart.</p>
+        )}
+        <Link
+          className="Link"
+          to={"/collections"}
+          onClick={() => dispatch(cartOpen())}
+        >
+          <p className="shopping-btn">Continue Shopping &rarr;</p>
+        </Link>
       </div>
     </div>
   );
