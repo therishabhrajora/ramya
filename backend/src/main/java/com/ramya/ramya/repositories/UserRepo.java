a87863c5ca1b2b608be8f02b0386aba77a2c95f0
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-28 22:46:41 +0530

    save user

--- backend/src/main/java/com/ramya/ramya/repositories/UserRepo.java
+++ backend/src/main/java/com/ramya/ramya/repositories/UserRepo.java
@@ -3,9 +3,9 @@ package com.ramya.ramya.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ramya.ramya.entities.Users;
+import com.ramya.ramya.entities.User;
 
 @Repository
-public interface UserRepo extends JpaRepository<Users, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
