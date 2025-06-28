2c28ba170e6b44241f7b47367c0e92f74d2d278a
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-28 09:39:22 +0530

    filter by checkbox added

--- frontend/src/components/homePage/NavBar.jsx
+++ frontend/src/components/homePage/NavBar.jsx
@@ -13,6 +13,8 @@ import {
   womenCollectionClose,
   cartOpen,
 } from "../../slices/NavBarSlice";
+
+
 import { useDispatch, useSelector } from "react-redux";
 import MenHoverSection from "../HoverComponents/MenHoverSection";
 import WomenHoverSection from "../HoverComponents/WomenHoverSection";
@@ -26,82 +28,89 @@ function NavBar() {
   const isWomenCollectionOpen = useSelector(
     (state) => state.navBar.womenCollectionOpen
   );
-  
+
+  const cartProducts = useSelector((state) => state.product.cartProducts);
+  const cartCount=cartProducts.length;
 
   return (
-
-      <div className="navBar baseBackgroundColor">
-        <div className="logo">
-          <Link className="Link" to="/collections">
-            RamYa
-          </Link>
-        </div>
-        <div className="menuBar baseTextColor">
-          <li
-            className="men"
-            onMouseEnter={() => {
-              dispatch(menCollectionOpen());
-              dispatch(increasecount());
-            }}
-            onMouseLeave={() => dispatch(menCollectionClose())}
-          >
-            <Link className="Link" to="/collections/men">
-              Men
-            </Link>
-          </li>
-          {isMenCollectionOpen ? <MenHoverSection /> : null}
-          <li className="women">
-            <Link
-              className="Link"
-              to="/collections/women"
-              onMouseEnter={() => {
-                dispatch(womenCollectionOpen());
-                dispatch(increasecount());
-              }}
-              onMouseLeave={() => dispatch(womenCollectionClose())}
-            >
-              Women
-            </Link>
-          </li>
-
-          {isWomenCollectionOpen ? <WomenHoverSection /> : null}
-          <li className="exoflex">
-            <Link className="Link" to="/collections/ecoflex">
-              Ecoflex
-            </Link>
-          </li>
-          <li className="stethoscope">
-            <Link className="Link" to="/collections/stethoscope">
-              Stethoscope
-            </Link>
-          </li>
-          <li className="trackOrder">
-            <Link className="Link" to="/collections/track-order">
-              Track Order
-            </Link>
-          </li>
-          <li className="bulkOrder">
-            <Link className="Link" to="/collections/bulk-order">
-              Bulk Order
-            </Link>
-          </li>
-        </div>
-        <div className="cartMenu">
-          <li className="search">
-            <IoSearchOutline />
-          </li>
-          <li className="login">
-            <Link className="Link" to="/account/login">
-              <CgProfile />
-            </Link>
-          </li>
-          <li className="cart" onClick={() => dispatch(cartOpen())}>
-            <IoCartOutline />
-          </li>
-        </div>
+    <div className="navBar baseBackgroundColor">
+      <div className="logo">
+        <Link className="Link" to="/collections">
+          RamYa
+        </Link>
       </div>
-     
-  
+      <div className="menuBar baseTextColor">
+        <li
+          className="men"
+          onMouseEnter={() => {
+            dispatch(menCollectionOpen());
+            
+          }}
+          // onMouseLeave={() => dispatch(menCollectionClose())}
+        >
+          <Link className="Link" to="/collections/men">
+            Men
+          </Link>
+        </li>
+        {isMenCollectionOpen ? <MenHoverSection /> : null}
+        <li className="women">
+          <Link
+            className="Link"
+            to="/collections/women"
+            onMouseEnter={() => {
+              dispatch(womenCollectionOpen());
+             
+            }}
+            // onMouseLeave={() => dispatch(womenCollectionClose())}
+          >
+            Women
+          </Link>
+        </li>
+
+        {isWomenCollectionOpen ? <WomenHoverSection /> : null}
+        <li className="exoflex">
+          <Link className="Link" to="/collections/ecoflex">
+            Ecoflex
+          </Link>
+        </li>
+        <li className="stethoscope">
+          <Link className="Link" to="/collections/stethoscope">
+            Stethoscope
+          </Link>
+        </li>
+        <li className="trackOrder">
+          <Link className="Link" to="/collections/track-order">
+            Track Order
+          </Link>
+        </li>
+        <li className="bulkOrder">
+          <Link className="Link" to="/collections/bulk-order">
+            Bulk Order
+          </Link>
+        </li>
+      </div>
+      <div className="cartMenu">
+        <li className="search">
+          <IoSearchOutline />
+        </li>
+        <li className="login">
+          <Link className="Link" to="/account/login">
+            <CgProfile />
+          </Link>
+        </li>
+        <li className="cart" onClick={() => dispatch(cartOpen())}>
+          {cartCount > 0 ? (
+            <div className="cartCount">
+              <span>{cartCount}</span>
+              <IoCartOutline  className="cart-icon"/>
+            </div>
+          ) : (
+            <IoCartOutline />
+          )}
+        </li>
+       
+      </div>
+    </div>
   );
 }
 
--- frontend/src/components/menCollections/FilterCollection.jsx
+++ frontend/src/components/menCollections/FilterCollection.jsx
@@ -6,7 +6,13 @@ import { useState } from "react";
 import { addToCart } from "../../slices/ProductSlice";
 function FilterCollection() {
   const dispatch = useDispatch();
+  const [selectCheckBox, setSelectCheckBox] = useState([]);
   const allCollections = useSelector((state) => state.product.products);
+  const colors = useSelector((state) => state.product.colors);
+  const pockets = useSelector((state) => state.product.pockets);
+  const gender = useSelector((state) => state.product.gender);
+  const type = useSelector((state) => state.product.type);
+
   const menProducts = allCollections
     .filter((product) => product.gender === "men")
     .slice(0, -4);
@@ -21,8 +27,32 @@ function FilterCollection() {
     ratingHighToLow: (a, b) => b.rating - a.rating,
     ratingLowToHigh: (a, b) => a.rating - b.rating,
   };
-  menProducts.sort(sortFunctions[sortBy]);
+  const sortedMenProducts = [...menProducts].sort(sortFunctions[sortBy]);
+  let filterproducts = sortedMenProducts.filter((product) => {
+    return (
+      selectCheckBox.length === 0 ||
+      selectCheckBox.includes(product.color) ||
+      selectCheckBox.includes(String(product.pocket)) ||
+      selectCheckBox.includes(product.gender) ||
+      selectCheckBox.includes(product.category)
+    );
+  });
 
+  
+
+  
+
+  let handleCheckBoxes = (e) => {
+    let value = e.target.value;
+    console.log(value);
+    if (e.target.checked) {
+      setSelectCheckBox((prev) => [...prev, value]);
+    } else {
+      setSelectCheckBox((prev) => prev.filter((item) => item !== value));
+    }
+  };
+
+  console.log(selectCheckBox);
   return (
     <div className="filterCollectionContainer">
       <p className="sortBySection">
@@ -53,38 +83,17 @@ function FilterCollection() {
               <IoMdArrowDropup />
             </h3>
             <ul className="colourList">
-              <li>
-                <label htmlFor="navy">navy</label>
-                <input type="checkbox" id="navy" />
-              </li>
-              <li>
-                <label htmlFor="Wine">Wine</label>
-                <input type="checkbox" id="Wine" />
-              </li>
-              <li>
-                <label htmlFor="Black">Black</label>
-                <input type="checkbox" id="Black" />
-              </li>
-              <li>
-                <label htmlFor="Forest Green">Forest Green</label>
-                <input type="checkbox" id="Forest Green" />
-              </li>
-              <li>
-                <label htmlFor="Ciel Blue">Ciel Blue</label>
-                <input type="checkbox" id="Ciel Blue" />
-              </li>
-              <li>
-                <label htmlFor="heather grey">heather grey</label>
-                <input type="checkbox" id="heather grey" />
-              </li>
-              <li>
-                <label htmlFor="Olive">Olive</label>
-                <input type="checkbox" id="Olive" />
-              </li>
-              <li>
-                <label htmlFor="steel gray">steel gray</label>
-                <input type="checkbox" id="steel gray" />
-              </li>
+              {colors.map((color) => (
+                <li key={color.value}>
+                  <label htmlFor={color.label}>{color.label}</label>
+                  <input
+                    type="checkbox"
+                    id={color.label}
+                    value={color.value}
+                    onChange={handleCheckBoxes}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <div className="filterByPockets">
@@ -93,18 +102,17 @@ function FilterCollection() {
               <IoMdArrowDropup />
             </h3>
             <ul className="pocketsList">
-              <li>
-                <label htmlFor=" 5 pockets"> 5 pockets</label>
-                <input type="checkbox" id=" 5 pockets" />
-              </li>
-              <li>
-                <label htmlFor="8 pockets">8 pockets</label>
-                <input type="checkbox" id="8 pockets" />
-              </li>
-              <li>
-                <label htmlFor="10 pockets">10 pockets</label>
-                <input type="checkbox" id="10 pockets" />
-              </li>
+              {pockets.map((pocket) => (
+                <li key={pocket}>
+                  <label htmlFor={pocket}>{pocket}</label>
+                  <input
+                    type="checkbox"
+                    id={pocket}
+                    value={pocket}
+                    onChange={handleCheckBoxes}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <div className="filterByGender">
@@ -113,14 +121,17 @@ function FilterCollection() {
               <IoMdArrowDropup />
             </h3>
             <ul className="genderList">
-              <li>
-                <label htmlFor="Male">Male</label>
-                <input type="checkbox" id="Male" />
-              </li>
-              <li>
-                <label htmlFor="Female">Female</label>
-                <input type="checkbox" id="Female" />
-              </li>
+              {gender.map((gen) => (
+                <li key={gen}>
+                  <label htmlFor={gen}>{gen}</label>
+                  <input
+                    type="checkbox"
+                    id={gen}
+                    value={gen}
+                    onChange={handleCheckBoxes}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <div className="filterByFabric">
@@ -129,19 +140,23 @@ function FilterCollection() {
               <IoMdArrowDropup />
             </h3>
             <ul className="fabricList">
-              <li>
-                <label htmlFor="Classic">Classic</label>
-                <input type="checkbox" id="Classic" />
-              </li>
-              <li>
-                <label htmlFor="ecoflex">ecoflex</label>
-                <input type="checkbox" id="ecoflex" />
-              </li>
+              {type.map((t) => (
+                <li key={t}>
+                  <label htmlFor={t}>{t}</label>
+                  <input
+                    type="checkbox"
+                    id={t}
+                    value={t}
+                    onChange={handleCheckBoxes}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
         </section>
         <section className="filterResults">
-          {menProducts.map((scrub) => (
+
+          {filterproducts.length==0 ? menProducts.map((scrub) => (
             <div className="scrubCard" key={scrub.id}>
               <div className="scrubImage">
                 <img src={scrub.image} alt={scrub.name} width="235px" />
@@ -154,10 +169,37 @@ function FilterCollection() {
                 <h3>{scrub.name}</h3>
                 <p>Price: ₹{scrub.price}</p>
                 <p>Color: {scrub.color}</p>
-                <div className="addToCartbtn" onClick={()=>dispatch(addToCart(scrub))}>Add to Cart</div>
+                <div
+                  className="addToCartbtn"
+                  onClick={() => dispatch(addToCart(scrub))}
+                >
+                  Add to Cart
+                </div>
+              </div>
+            </div>
+          )) :filterproducts.map((scrub) => (
+            <div className="scrubCard" key={scrub.id}>
+              <div className="scrubImage">
+                <img src={scrub.image} alt={scrub.name} width="235px" />
+              </div>
+              <div className="scrubDetails">
+                <div className="rating">
+                  <h5>Ecoflex</h5>
+                  <h5>{scrub.rating}</h5>
+                </div>
+                <h3>{scrub.name}</h3>
+                <p>Price: ₹{scrub.price}</p>
+                <p>Color: {scrub.color}</p>
+                <div
+                  className="addToCartbtn"
+                  onClick={() => dispatch(addToCart(scrub))}
+                >
+                  Add to Cart
+                </div>
               </div>
             </div>
           ))}
+
         </section>
       </div>
     </div>
