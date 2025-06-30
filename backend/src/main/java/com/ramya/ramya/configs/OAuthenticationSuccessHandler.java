d171a41e083c032c95ce917e3a9d6d5e173f4ffb
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-30 11:54:17 +0530

    add AuthenticationSuccessHandler

---
+++ backend/src/main/java/com/ramya/ramya/configs/OAuthenticationSuccessHandler.java
@@ -0,0 +1,50 @@
+package com.ramya.ramya.configs;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.ramya.ramya.entities.User;
+import com.ramya.ramya.repositories.UserRepo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class OAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
+    private final UserRepo userRepo;
+    private final JwtUtil jwtUtil;
+
+    public OAuthenticationSuccessHandler(UserRepo userRepo,JwtUtil jwtUtil){
+        this.jwtUtil=jwtUtil;
+        this.userRepo=userRepo;
+    }
+
+    Logger logger=LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+      var onAuthenticationToken=(OAuth2AuthenticationToken)authentication;
+      String authorizedClientRegisterationId=onAuthenticationToken.getAuthorizedClientRegistrationId();
+      var oauthUser=(DefaultOAuth2User)authentication.getPrincipal();
+
+      String jwtToken=jwtUtil.generateToken((UserDetails)authentication.getPrincipal());
+      response.setHeader("Authentication", "Bearer " + jwtToken);
+      oauthUser.getAttributes().forEach((key, value) -> {
+        logger.info(key + ": " + value); // Uncomment if logger is defined
+      });
+
+    }
+
+    
+}
