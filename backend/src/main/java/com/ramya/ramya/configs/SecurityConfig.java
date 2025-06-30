78e272338ee916c726260ee10ab3f81706ad69b9
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-30 12:27:43 +0530

    add some configs

--- backend/src/main/java/com/ramya/ramya/configs/SecurityConfig.java
+++ backend/src/main/java/com/ramya/ramya/configs/SecurityConfig.java
@@ -1,27 +1,48 @@
 package com.ramya.ramya.configs;
 
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+
+import com.ramya.ramya.services.CustomUserDetailService;
 
 @Configuration
 @EnableAutoConfiguration
 public class SecurityConfig {
-    
+
+    private JwtRequestFilter jwtRequestFilter;
+    private CustomUserDetailService userDetailService;
+    private OAuthenticationSuccessHandler oAuthenticationSuccessHandler;
+
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter, CustomUserDetailService userDetailService,
+            OAuthenticationSuccessHandler oAuthenticationSuccessHandler) {
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.userDetailService = userDetailService;
+        this.oAuthenticationSuccessHandler = oAuthenticationSuccessHandler;
+    }
 
     @Bean
-    public SecurityFilterChain sequrityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain sequrityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/**").permitAll()
-            .anyRequest().authenticated());
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpbasic -> httpbasic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated())
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration config = new CorsConfiguration();
@@ -32,11 +53,40 @@ public class SecurityConfig {
             return config;
         }));
 
+        http.oauth2Login(oauth -> {
+            oauth.loginPage("/collections/account");
+            oauth.successHandler(oAuthenticationSuccessHandler);
+        });
+
+        http.logout(logout -> logout
+                .logoutUrl("/collections/logout")
+                .logoutSuccessUrl("/collections/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll());
+
+        http.sessionManagement(session -> session
+                .invalidSessionUrl("/login?invalidSession=true"));
+
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
