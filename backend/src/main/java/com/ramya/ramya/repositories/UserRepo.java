70ded288fbef3a3e1b9edd43bcbf47bb28f9ca6f
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-30 10:52:29 +0530

    change id type

--- backend/src/main/java/com/ramya/ramya/repositories/UserRepo.java
+++ backend/src/main/java/com/ramya/ramya/repositories/UserRepo.java
@@ -1,11 +1,17 @@
 package com.ramya.ramya.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ramya.ramya.entities.User;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+
 
 }
