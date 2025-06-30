4aa09c9d9ed98490806312c8eb81cc580d21aaf7
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-06-30 10:54:50 +0530

    update saveUser

--- backend/src/main/java/com/ramya/ramya/services/UserService.java
+++ backend/src/main/java/com/ramya/ramya/services/UserService.java
@@ -1,7 +1,14 @@
 package com.ramya.ramya.services;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ramya.ramya.entities.User;
 import com.ramya.ramya.repositories.UserRepo;
@@ -10,16 +17,33 @@ import com.ramya.ramya.repositories.UserRepo;
 public class UserService {
 
     private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public void saveUser(User user) {
-       userRepo.save(user);
+    public ResponseEntity<Map<String, String>> saveUser( User user) {
+        Optional<User> userExist = userRepo.findByEmail(user.getEmail());
+        System.out.println("this is user exist "+userExist);
+        if (userExist.isPresent()) {
+            System.out.println("inside if");
+            return ResponseEntity
+                .badRequest()
+                .body(Map.of("message", "User is already present"));
+        } else {
+            System.out.println(
+                "inside else"
+            );
+            String id = UUID.randomUUID().toString();
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setId(id);
+            userRepo.save(user);
+            return ResponseEntity
+                .ok(Map.of("message", "User registered successfully"));
+        }
     }
-
-
 
 }
