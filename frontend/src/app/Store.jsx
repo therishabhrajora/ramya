a62a9e606264ad76366db53e72a1f14601d36647
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-02 23:51:22 +0530

    add auth recuder

--- frontend/src/app/Store.jsx
+++ frontend/src/app/Store.jsx
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navbarReducer from "../slices/NavBarSlice";
 import productSlice from "../slices/ProductSlice";
+import authSlice from "../slices/AuthSlice";
+
 
 export const store = configureStore({
   reducer: {
     navBar: navbarReducer,
     product: productSlice,
+    auth:authSlice
   },
 });
