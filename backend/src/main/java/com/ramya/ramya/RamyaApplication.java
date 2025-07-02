83c5f154ee8447a44eecb484e13797a99cb2762e
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-02 23:48:50 +0530

    format code

--- backend/src/main/java/com/ramya/ramya/RamyaApplication.java
+++ backend/src/main/java/com/ramya/ramya/RamyaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RamyaApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RamyaApplication.class, args);
 	}
