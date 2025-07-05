71df6137950b12f099b144b05c4249508dcaad59
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-05 23:53:47 +0530

    add Image Constants

--- backend/src/main/java/helper/AppConstants.java
+++ backend/src/main/java/helper/AppConstants.java
@@ -1,11 +1,16 @@
 package helper;
 
 public class AppConstants {
-    public static final String APPLICATION_NAME = "My Spring Boot App";
+    public static final String APPLICATION_NAME = "RamYa";
     public static final String DATE_FORMAT = "dd-MM-yyyy";
     public static final String TIMEZONE = "Asia/Kolkata";
     public static final int DEFAULT_PAGE_SIZE = 20;
     public static final String PROJECT_NAME = "ramya";
+    public static final int PRODUCT_IMAGE_WIDTH=500;
+    public static final int PRODUCT_IMAGE_HEIGHT=500;
+    public static final String PRODUCT_IMAGE_CROP="fill";
+
+
     
 
     private AppConstants() {
