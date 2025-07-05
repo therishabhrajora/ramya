73adcffe9017a98b86e3ccfeaa591691d11339aa
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-05 19:55:44 +0530

    add constants

--- backend/src/main/java/com/ramya/ramya/configs/JwtUtil.java
+++ backend/src/main/java/com/ramya/ramya/configs/JwtUtil.java
@@ -6,22 +6,27 @@ import java.util.Date;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import helper.AppConstants;
+import helper.SecurityConstants;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    Key key = Keys.hmacShaKeyFor("your-256-bit-secret-your-256-bit-secret".getBytes());
+    String JWT_SECRET=SecurityConstants.JWT_SECRET;
+    Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    String project=AppConstants.PROJECT_NAME;
 
-    public String generateToken(UserDetails userDetails){
-        return Jwts.builder()
-            .claim("sub", userDetails.getUsername())
-            .claim("iss", "ramya")
-            .claim("iat", new Date().getTime())
-            .claim("exp", System.currentTimeMillis()+1000*60*60*10)
+    public String generateToken(UserDetails userDetails) {
+    return Jwts.builder()
+            .claim("role", userDetails.getAuthorities().iterator().next().getAuthority())
+            .setSubject(userDetails.getUsername())
+            .setIssuer(project)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
             .signWith(key)
             .compact();
-    }
+}
 
 
     public String extractUsername(String token){
@@ -30,7 +35,6 @@ public class JwtUtil {
 
     public boolean validateToken(String token,UserDetails userDetails){
         String username = extractUsername(token);
-        System.out.println("this user exist ===" + username);
         return (username.equals(userDetails.getUsername()));
     }
 
