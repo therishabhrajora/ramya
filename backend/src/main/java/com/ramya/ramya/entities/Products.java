a8515dd4494524960069ca593ef5d9f2174cbc9b
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-05 19:57:37 +0530

    validate updates

--- backend/src/main/java/com/ramya/ramya/entities/Products.java
+++ backend/src/main/java/com/ramya/ramya/entities/Products.java
@@ -1,9 +1,9 @@
 package com.ramya.ramya.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,23 +19,25 @@ import lombok.Setter;
 @Data
 public class Products {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    String productId;
+    private String productId;
     @NotBlank(message = "must be filled")
-    String name;
+    private String name;
+    
+    private String imageLink;
     @NotBlank(message = "must be filled")
-    String imageLink;
+    private String gender;
     @NotBlank(message = "must be filled")
-    String gender;
+    private String category;
     @NotBlank(message = "must be filled")
-    String category;
-    @NotBlank(message = "must be filled")
-    String color;
-    @NotBlank(message = "must be filled")
-    int price;
-    @NotBlank(message = "must be filled")
-    int pocket;
-    @NotBlank(message = "must be filled")
-    float rating;
+    private String color;
+
+    @Min(value = 1, message = "Pocket must be at least 1")
+    private float price;
+
+    @Min(value = 1, message = "Pocket must be at least 1")
+    private int pocket;
+
+    @Min(value = 1, message = "Pocket must be at least 1")
+    private float rating;
 
 }
