349d7ce52a2beb52a32b536c8b4554d6b7f8d8e2
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-05 19:55:13 +0530

    .

--- backend/src/main/java/com/ramya/ramya/configs/JwtRequestFilter.java
+++ backend/src/main/java/com/ramya/ramya/configs/JwtRequestFilter.java
@@ -11,42 +11,46 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.ramya.ramya.services.CustomUserDetailService;
 
-import jakarta.servlet.FilterChain;
+import helper.SecurityConstants;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
+    String header = SecurityConstants.HEADER_STRING;
+    String bearer = SecurityConstants.TOKEN_PREFIX;
 
     private final CustomUserDetailService userDetailService;
     private final JwtUtil jwtUtil;
 
-    public JwtRequestFilter(CustomUserDetailService userDetailService,JwtUtil jwtUtil){
-        this.userDetailService=userDetailService;
-        this.jwtUtil=jwtUtil;
+    public JwtRequestFilter(CustomUserDetailService userDetailService, JwtUtil jwtUtil) {
+        this.userDetailService = userDetailService;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        final String authHeader=request.getHeader("Authorization");
-        String username=null;
-        String jwt=null;
-            if(authHeader!=null && authHeader.startsWith("Bearer")){
-                jwt=authHeader.substring(7);
-                username=jwtUtil.extractUsername(jwt);
-            }
+        final String authHeader = request.getHeader(header);
+        String username = null;
+        String jwt = null;
+        if (authHeader != null && authHeader.startsWith(bearer)) {
+            jwt = authHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
 
-            if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-                UserDetails userDetails=this.userDetailService.loadUserByUsername(username);
-                if(jwtUtil.validateToken(jwt, userDetails)){
-                    UsernamePasswordAuthenticationToken authToken=new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
-            filterChain.doFilter(request, response);
+        }
+        filterChain.doFilter(request, response);
     }
-    
+
 }
