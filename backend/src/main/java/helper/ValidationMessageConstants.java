d2531936db4e34e6ba8a9251be1ed5b1d8a793b3
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-06 13:43:53 +0530

    add ValidationConstants

---
+++ backend/src/main/java/helper/ValidationMessageConstants.java
@@ -0,0 +1,21 @@
+package helper;
+
+public class ValidationMessageConstants {
+    public static final String FIELD_REQUIRED = "This field is required.";
+    public static final String INVALID_EMAIL = "Please enter a valid email address.";
+    public static final String PASSWORD_TOO_SHORT = "Password must be at least 8 characters.";
+    public static final String INVALID_PRICE = "Price must be greater than 0.";
+    public static final String INVALID_RATING = "Rating must be between 1 and 5.";
+    public static final String USERNAME_REQUIRED = "Username is required.";
+    public static final String EMAIL_REQUIRED = "Email is required.";
+    public static final String PASSWORD_REQUIRED = "Password is required.";
+    public static final String PASSWORD_TOO_WEAK = "Password must contain a number, uppercase, and symbol.";
+    public static final String INVALID_INPUT = "Invalid input.";
+    public static final String FIELD_TOO_SHORT = "Input is too short.";
+    public static final String FIELD_TOO_LONG = "Input is too long.";
+    public static final String VALUE_MUST_BE_POSITIVE = "Value must be greater than 0.";
+    public static final String PHONE_NUMBER_MUST_BE_10_DIGIT="Phone number must be 10 digits";
+
+    private ValidationMessageConstants() {
+    }
+}
