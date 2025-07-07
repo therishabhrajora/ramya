509c6f6eb47cc1ab8d68a28abe7b2ed6aaa3d53a
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-07 23:55:22 +0530

    create a profile model

---
+++ frontend/src/components/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { IoIosLogOut } from "react-icons/io";
+import "../../styles/profile/profile.css";
+import { useDispatch } from "react-redux";
+import { profileOpen } from "../../slices/NavBarSlice";
+import { logout } from "../../slices/AuthSlice";
+
+function Profile() {
+  const userStr = localStorage.getItem("user");
+  const dispatch=useDispatch();
+  const user = userStr ? JSON.parse(userStr) : null;
+  const name = user ? (user.firstName + " " + user.lastName) : "";
+  const email = user ? user.email : "";
+  const navigate = useNavigate();
+  
+
+  const handleLogout = () => {
+    dispatch(logout());
+    console.log("removed tokens");
+    dispatch(profileOpen())
+    navigate("/collections");
+  };
+
+  return (
+    <ul className="profile">
+      <li className="name">{name}</li>
+      <li className="email">{email}</li>
+      <li className="orders">Orders</li>
+      <li className="logout" onClick={handleLogout}>
+        <span> logout </span>
+        <IoIosLogOut />
+      </li>
+    </ul>
+  );
+}
+
+export default Profile;
