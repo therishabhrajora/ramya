7a26e24bfc4bc987eb0a5551b7488331af4d8137
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-07 23:54:31 +0530

    add profile model

--- frontend/src/components/homePage/NavBar.jsx
+++ frontend/src/components/homePage/NavBar.jsx
@@ -1,6 +1,10 @@
 import { IoSearchOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
+import { TiFolderAdd } from "react-icons/ti";
+import { GrUserAdmin } from "react-icons/gr";
 import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
+import { IoMdLogIn } from "react-icons/io";
 import "../../index.css";
 import "../../styles/homepage/navBar.css";
 import "../../styles/homepage/Cart.css";
@@ -9,16 +13,23 @@ import {
   menCollectionOpen,
   womenCollectionOpen,
   cartOpen,
+  profileOpen,
+  closeProfile,
   menCollectionClose,
-  womenCollectionClose
+  womenCollectionClose,
 } from "../../slices/NavBarSlice";
 
 import { useDispatch, useSelector } from "react-redux";
 import MenHoverSection from "../HoverComponents/MenHoverSection";
 import WomenHoverSection from "../HoverComponents/WomenHoverSection";
+import Profile from "../profile/profile";
 
 function NavBar() {
   const dispatch = useDispatch();
+
+  const role = localStorage.getItem("role");
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const isProfileOpen = useSelector((state) => state.navBar.profileOpen);
 
   const isMenCollectionOpen = useSelector(
     (state) => state.navBar.menCollectionOpen
@@ -26,7 +37,6 @@ function NavBar() {
   const isWomenCollectionOpen = useSelector(
     (state) => state.navBar.womenCollectionOpen
   );
-
   const cartProducts = useSelector((state) => state.product.cartProducts);
   const cartCount = cartProducts.length;
 
@@ -38,61 +48,121 @@ function NavBar() {
         </Link>
       </div>
       <div className="menuBar baseTextColor">
-        <li
-          className="men"
-          onMouseEnter={() => {
-            dispatch(menCollectionOpen());
-          }}
+        {/* Men Menu */}
+        <div
+          className="menu-item-wrapper"
+          onMouseEnter={() => dispatch(menCollectionOpen())}
           onMouseLeave={() => dispatch(menCollectionClose())}
         >
-          <Link className="Link" to="/collections/men">
-            Men
-          </Link>
-        </li>
-        {isMenCollectionOpen ? <MenHoverSection /> : null}
-        <li className="women">
-          <Link
-            className="Link"
-            to="/collections/women"
-            onMouseEnter={() => {
-              dispatch(womenCollectionOpen());
-            }}
-            onMouseLeave={() => dispatch(womenCollectionClose())}
-          >
-            Women
-          </Link>
-        </li>
+          <ul className="menu-list">
+            <li className="men">
+              <Link className="Link" to="/collections/men">
+                Men
+              </Link>
+            </li>
+          </ul>
+          {isMenCollectionOpen && (
+            <div className="hover-wrapper">
+              <MenHoverSection />
+            </div>
+          )}
+        </div>
 
-        {isWomenCollectionOpen ? <WomenHoverSection /> : null}
-        <li className="exoflex">
-          <Link className="Link" to="/collections/ecoflex">
-            Ecoflex
-          </Link>
-        </li>
-        <li className="stethoscope">
-          <Link className="Link" to="/collections/stethoscope">
-            Stethoscope
-          </Link>
-        </li>
-        <li className="trackOrder">
-          <Link className="Link" to="/collections/track-order">
-            Track Order
-          </Link>
-        </li>
-        <li className="bulkOrder">
-          <Link className="Link" to="/collections/bulk-order">
-            Bulk Order
-          </Link>
-        </li>
+        <div
+          className="menu-item-wrapper"
+          onMouseEnter={() => dispatch(womenCollectionOpen())}
+          onMouseLeave={() => dispatch(womenCollectionClose())}
+        >
+          <ul className="menu-list">
+            <li className="women">
+              <Link className="Link" to="/collections/women">
+                Women
+              </Link>
+            </li>
+          </ul>
+          {isWomenCollectionOpen && (
+            <div className="hover-wrapper">
+              <WomenHoverSection />
+            </div>
+          )}
+        </div>
+
+        {/* Static links (no hover) */}
+        <ul className="menu-list">
+          <li className="exoflex">
+            <Link className="Link" to="/collections/ecoflex">
+              Ecoflex
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li className="stethoscope">
+            <Link className="Link" to="/collections/stethoscope">
+              Stethoscope
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li className="trackOrder">
+            <Link className="Link" to="/collections/track-order">
+              Track Order
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li className="bulkOrder">
+            <Link className="Link" to="/collections/bulk-order">
+              Bulk Order
+            </Link>
+          </li>
+        </ul>
       </div>
+
       <div className="cartMenu">
         <li className="search">
           <IoSearchOutline />
         </li>
+
         <li className="login">
-          <Link className="Link" to="/collections/account">
-            <CgProfile />
-          </Link>
+          {console.log("this is openProfile: ", isProfileOpen)}
+          {isLoggedIn ? (
+            role === "ROLE_ADMIN" ? (
+              <ul
+                className="admin-section"
+                onMouseEnter={() => dispatch(profileOpen())}
+                onMouseLeave={() => dispatch(closeProfile())}
+              >
+                <li
+                  className="Link admin-profile"
+                  // onMouseEnter={() => dispatch(profileOpen())}
+                >
+                  <MdOutlineAdminPanelSettings />
+                </li>
+                {isProfileOpen && <Profile />}
+                <Link
+                  className="Link add-products"
+                  to="/collections/admin/add-products"
+                >
+                  <TiFolderAdd />
+                </Link>
+              </ul>
+            ) : (
+              <ul
+                onMouseEnter={() => dispatch(profileOpen())}
+                onMouseLeave={() => dispatch(closeProfile())}
+              >
+                {console.log("this is openProfile: ", isProfileOpen)}
+                <li className="Link user-profile">
+                  <CgProfile />
+                </li>
+                {isProfileOpen && <Profile />}
+              </ul>
+            )
+          ) : (
+            <Link className="Link" to="/collections/account">
+              <IoMdLogIn />
+            </Link>
+          )}
         </li>
         <li className="cart" onClick={() => dispatch(cartOpen())}>
           {cartCount > 0 ? (
