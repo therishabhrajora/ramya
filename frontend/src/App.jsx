7a694153539c007b82346f1bad311771313ca54d
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-12 15:06:01 +0530

    add default route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,6 +38,7 @@ function App() {
      
       {isCartOpen ? <Cart /> : null}
       <Routes>
+        <Route path="/" element={<Navigate to="/collections" />} />
         <Route path="/collections" element={<HomePage />} />
         <Route path="/collections/men" element={<MenCollections />} />
         <Route path="/collections/women" element={<WomenCollections />} />
