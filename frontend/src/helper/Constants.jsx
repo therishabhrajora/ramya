37ce49ecc3f92af910b69de49b07a0957babf455
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-18 00:24:54 +0530

    add helper for constants

---
+++ frontend/src/helper/Constants.jsx
@@ -0,0 +1,7 @@
+export   const DATABASE_ORIGIN = process.env.DATABASE_ORIGIN;
+export const  ENDPOINTS={
+    addProducts:`${DATABASE_ORIGIN}/collections/admin/add-products`,
+    login: `${DATABASE_ORIGIN}/collections/login`,
+    products:  `${DATABASE_ORIGIN}/collections/products`,
+    register:`${DATABASE_ORIGIN}/collections/register`,
+}
