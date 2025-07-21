c658e7716a79b63e804f7faacc7e0da595ed41c4
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-21 13:36:52 +0530

    .

--- frontend/src/helper/Constants.js
+++ frontend/src/helper/Constants.js
@@ -1,7 +1,7 @@
 export   const DATABASE_ORIGIN = "https://ramya-2c4k.onrender.com/"
 export const  ENDPOINTS={
-    addProducts:`${DATABASE_ORIGIN}/collections/admin/add-products`,
-    login: `${DATABASE_ORIGIN}/collections/login`,
-    products:  `${DATABASE_ORIGIN}/collections/products`,
-    register:`${DATABASE_ORIGIN}/collections/register`,
+    addProducts:`${DATABASE_ORIGIN}collections/admin/add-products`,
+    login: `${DATABASE_ORIGIN}collections/login`,
+    products:  `${DATABASE_ORIGIN}collections/products`,
+    register:`${DATABASE_ORIGIN}collections/register`,
 }
