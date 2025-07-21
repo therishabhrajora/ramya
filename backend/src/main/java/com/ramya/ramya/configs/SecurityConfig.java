0420091dea74f97bf971ec979384ab45330477a6
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-21 13:17:42 +0530

    .

--- backend/src/main/java/com/ramya/ramya/configs/SecurityConfig.java
+++ backend/src/main/java/com/ramya/ramya/configs/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
             config.addAllowedHeader("*");
             config.addAllowedMethod("*");
             return config;
-        }))
+        }));
        
 
 
