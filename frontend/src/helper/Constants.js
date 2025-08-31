11a3c3e63ff4b162c1161e476cdc87a9baa1c884
therishabhrajora <rishabhrajora20gmail.com>
2025-09-01 01:16:00 +0530

    .

--- frontend/src/helper/Constants.js
+++ frontend/src/helper/Constants.js
@@ -1,5 +1,5 @@
-// export   const DATABASE_ORIGIN = "https://ramya-2c4k.onrender.com/"
-export   const DATABASE_ORIGIN = "http://localhost:9090/"
+export   const DATABASE_ORIGIN = "https://ramya-2c4k.onrender.com/"
+// export   const DATABASE_ORIGIN = "http://localhost:9090/"
 export const  ENDPOINTS={
     addProducts:`${DATABASE_ORIGIN}collections/admin/add-products`,
     login: `${DATABASE_ORIGIN}collections/login`,
